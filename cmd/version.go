bc674c271f917c21158bfa41e7b7531bacac123a
muffin <muffliato1998@outlook.com>
2024-11-22 14:50:52 +0800

    feat: modify "version" output

--- cmd/version.go
+++ cmd/version.go
@@ -12,19 +12,7 @@ var versionCmd = &cobra.Command{
 	Short: "Show version.",
 	Args:  cobra.ExactArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		// 打印标题
-		fmt.Print("\n========================================\n")
-		fmt.Print("              SSHE Version             \n")
-		fmt.Print("========================================\n")
-
-		// 打印版本信息，带颜色
 		fmt.Printf("Version:    \x1b[32m%s\x1b[0m\n", config.Version)
-		if config.Commit != "" {
-			fmt.Printf("Commit:     \x1b[33m%s\x1b[0m\n", config.Commit)
-		}
-
-		// 打印分隔符
-		fmt.Print("\n========================================\n")
 	},
 }
 
